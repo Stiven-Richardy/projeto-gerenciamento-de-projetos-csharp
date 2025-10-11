e4fc7ed059751a5f25c699c1ae1775d2043d87c7
Guilh3rme-M3ndes <guilheu2004@gmail.com>
2025-10-11 00:35:14 -0300

    added view function to tasks

--- projeto-gerenciamento-de-projetos/Program.cs
+++ projeto-gerenciamento-de-projetos/Program.cs
@@ -72,6 +72,7 @@ namespace projeto_gerenciamento_de_projetos
                     case 7:
                         break;
                     case 8:
+                        listarTarefas();
                         break;
                     case 9:
                         break;
@@ -142,7 +143,45 @@ namespace projeto_gerenciamento_de_projetos
 
         }
 
+        static void listarTarefas()
+        {
+            Utils.Titulo("LISTAR TAREFAS");
+            Console.Write("Informe o nome do projeto: ");
+            string nome = Console.ReadLine();
+            Projeto projeto = itens.buscar(new Projeto(nome));
+            if (projeto != null)
+            {
+                int qtdTarefas = projeto.totalAberta() + projeto.totalFechadas();
+                if (qtdTarefas > 0)
+                {
+                    Console.WriteLine($"Tarefas de {projeto.Nome}\n");
+                    foreach (Tarefa trf in projeto.Tarefas)
+                    {
+                        if (trf.Status != "Cancelada")
+                        {
+                            string prioridade = trf.Prioridade == 1 ? "Alta" : trf.Prioridade == 2 ? "Média" : "Baixa";
+                            Console.WriteLine($"Titulo: {trf.Titulo}\n" +
+                                $"Descrição: {trf.Descricao}\n" +
+                                $"Prioridade: {prioridade}\n" +
+                                $"Status: {trf.Status} \n" +
+                                $"Inicio: {trf.DataCriacao}\n");
+                            if (trf.DataConclusao != null)
+                            {
+                                Console.WriteLine($"Finalizado: {trf.DataConclusao}");
+                            }
+                        }
+                    }
+                    Utils.MensagemSucesso(" Listando tarefas abertas e concluidas...");
+                }
+                else
+                {
+                    Utils.MensagemErro("Nenhuma tarefa no projeto");
+                }
 
+            }
+            else
+                Utils.MensagemErro("Projeto não encontrado");
+        }
         static void pesquisarProjeto()
         {
             Utils.Titulo("PESQUISAR PROJETO (1/2)");
